66984f8da24b652cb513757bf0054a40de29f6a5
Victor de la Rocha <victor.roxa.r@gmail.com>
2022-04-25 15:13:50 +0200

    Implement use shortcuts decorator

---
+++ projects/ng-shortcuts/src/lib/decorators/use-shortcut.decorator.ts
@@ -0,0 +1,58 @@
+import { Subscription } from "rxjs";
+import { ListenerConstructor, StaticShortcutManager } from "../managers/static-shortcut-manager.service";
+
+export interface SubscriptionOptions {
+  unsubscribe: boolean;
+}
+
+/**
+ * Decorator that marks a component to listen to an event stream.
+ * @param listener The listener concrete implementation that listens and handles events.
+ * @param options The subscription options.
+ * @returns The use shortcuts decorator.
+ */
+export function UseShortcuts<
+  TComponent,
+  TListener extends ListenerConstructor<TComponent>
+>(
+  listener: TListener,
+  options: SubscriptionOptions = { unsubscribe: false }
+) {
+
+  const isValidLifehookFn = (fn: unknown): fn is () => void => {
+    return !!fn && typeof fn === 'function';
+  }
+
+  // The prototype is the decorated  component instance
+  return function({ prototype }: Function) {
+    // Keep the original lifehook functions
+    const { ngOnInit } = prototype;
+    const { ngOnDestroy } = prototype;
+
+    let subscription: Subscription;
+
+    // Override the init lifehook
+    // to initialize the listener.
+    prototype.ngOnInit = function() {
+      subscription = StaticShortcutManager.registerListener(listener, this);
+      if (isValidLifehookFn(ngOnInit)) {
+        ngOnInit.apply(this);
+      }
+    };
+
+    // Override the destroy lifehook
+    // to unsubscribe from the listener, if needed.
+    prototype.ngOnDestroy = function() {
+      const subscribed = !!subscription;
+      const shouldUnsubscribe = options.unsubscribe && subscribed;
+
+      if (shouldUnsubscribe) {
+        subscription.unsubscribe();
+      }
+
+      if (isValidLifehookFn(ngOnDestroy) && subscribed) {
+        ngOnDestroy.apply(this);
+      }
+    }
+  }
+}
