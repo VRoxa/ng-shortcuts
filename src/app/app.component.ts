d115953ad8eecc1cbfdd9547beb676c56367cd9b
Victor de la Rocha <victor.roxa.r@gmail.com>
2022-04-26 10:39:35 +0200

    Add excluded input in main application with decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { UseShortcuts } from 'projects/ng-shortcuts/src/lib/decorators/use-shortcut.decorator';
+import { ExcludeFromShortcuts } from 'projects/ng-shortcuts/src/public-api';
 import { AppListener } from './listeners/app.listener';
 
 @UseShortcuts<AppComponent, typeof AppListener>(AppListener)
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [
+    `
+    .content { display: flex; align-items: center;}
+    .excluded-element { margin: 2rem 0; }
+    .right { font-size: 5rem; padding: 0 1rem; }`
+  ]
 })
 export class AppComponent {
 
   public muted: boolean = false;
+  public counter: number = 0;
+
+  @ExcludeFromShortcuts()
+  @ViewChild('excluded') excluded?: ElementRef<HTMLButtonElement>;
   
   public sayHello = () => {
     console.info('Hello 🙋‍♂️');
+    ++this.counter;
   }
 
   public sayGoodbye = () => {
     console.info('Goodbye 🙋‍♂️');
+    --this.counter;
   }
 
   public toggleMute = () => {
